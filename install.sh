c9d8a427d20a8d54480d19dc54df1ef7d87773a7
indesege <indesege@gmail.com>
2015-08-30 23:06:13 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env sh
 
 # Installing base packages
-yes | sudo pacman -S vim xfce4-terminal xorg-xinit ttf-dejavu ttf-droid faience-icon-theme feh zsh
+yes | sudo pacman -S vim xfce4-terminal xorg-xinit ttf-dejavu ttf-droid faience-icon-theme feh zsh > /dev/null 2> /dev/null
 
 # Installing i3wm
 cd i3-smart-borders
-makepkg -si
+makepkg -si > /dev/null 2> /dev/null
+cd ..
 
 # Installing yaourt
-git clone https://aur.archlinux.org/package-query.git
+git clone https://aur.archlinux.org/package-query.git  > /dev/null 2> /dev/null
 cd package-query
-yes | makepkg -si
+yes | makepkg -si  > /dev/null 2> /dev/null
 cd ..
-git clone https://aur.archlinux.org/yaourt.git
+git clone https://aur.archlinux.org/yaourt.git  > /dev/null 2> /dev/null
 cd yaourt
-yes | makepkg -si
+yes | makepkg -si  > /dev/null 2> /dev/null
 cd ..
 rm -rf package-query yaourt
 
 # Installing oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"  > /dev/null 2> /dev/null
 
 # Modifying config
 sed -i -e s/robbyrussell/nanotech/g ~/.zshrc
@@ -32,16 +33,16 @@ echo "
 sudo usermod -s /usr/bin/zsh $USER
 
 # Installing yaourt packages
-yaourt -S --noconfirm dmenu2 j4-dmenu-desktop
+yaourt -S --noconfirm dmenu2 j4-dmenu-desktop  > /dev/null 2> /dev/null
 
-git clone git://github.com/i3/i3
+git clone git://github.com/i3/i3  > /dev/null 2> /dev/null
 cd i3
-curl https://raw.githubusercontent.com/ashinkarov/i3-extras/master/0x2493-patches/smart-border.patch | patch src/con.c
-make
+curl https://raw.githubusercontent.com/ashinkarov/i3-extras/master/0x2493-patches/smart-border.patch | patch src/con.c  > /dev/null 2> /dev/null
+make  > /dev/null 2> /dev/null
 cd ..
 rm -r i3
 
 # Copying config files
-cp -R configs/.* ~
-sudo cp -R share/* /usr/share
+cp -R configs/{.config,.gtkrc-2.0,.i3,.xinitrc} ~
+sudo cp -R share/{themes,wallpapers} /usr/share
 sudo chmod -R 777 /usr/share/wallpapers

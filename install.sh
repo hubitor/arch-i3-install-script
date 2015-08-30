949a5df61b76286a21a5bcc0c12ddca2e0147dc7
indesege <indesege@gmail.com>
2015-08-31 00:18:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo echo "Ok!"
 # Installing base packages
 echo "Installing base packages"
 (yes | sudo pacman -Rs vim-minimal || true) > /dev/null 2> /dev/null
-yes "" | sudo pacman -S vim xfce4-terminal xorg-xinit ttf-dejavu ttf-droid faience-icon-theme feh zsh > /dev/null 2> /dev/null
+yes "" | sudo pacman -S xorg-server vim xfce4-terminal xorg-xinit ttf-dejavu ttf-droid faience-icon-theme feh zsh > /dev/null 2> /dev/null
 
 # Installing i3wm
 echo "Installing i3wm from git with smart borders patch"

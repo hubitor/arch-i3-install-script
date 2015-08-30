71fe693068636b6542c2ee7d3b0b7908064314b7
indesege@gmail.com <indesege@gmail.com>
2015-08-30 16:49:15 +0300

    Install.sh semi-ready

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+read -p "Part your devices, make filesystems and mount to /mnt: "
+
+pacstrap -i /mnt base base-devel
+genfstab -U /mnt > /mnt/etc/fstab
+
+chmod +x ./install-chroot.sh
+cp install-chroot.sh /mnt
+cp locale.gen /mnt
+
+arch-chroot /mnt /bin/bash -x <<'EOF'
+su -
+./install-chroot.sh
+EOF
+
+rm /mnt/install-chroot.sh
+rm /mnt/locale.gen
+
+umount -R /mnt
+#reboot

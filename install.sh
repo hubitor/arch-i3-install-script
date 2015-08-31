0df7bf43620875051b077e6549482d6488aa24a4
indesege <indesege@gmail.com>
2015-08-31 03:53:47 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,13 +59,34 @@ sudo chmod -R 777 /usr/share/wallpapers
 echo "Updating pacman database"
 sudo pacman -Sy
 
-echo "Enabling autologin and X11 server start"
-sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
-echo "[Service]
-ExecStart=
-ExecStart=-/usr/bin/agetty --autologin $USER --noclear %I 38400 linux
-" | sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null
-echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' > ~/.zprofile
+read -p "Are you want to enable autologin? <Y/n> " prompt
+if [[ $prompt == "n" || $prompt == "no"  || $prompt == "N"  || $prompt == "No" ]]
+then
+  true
+else
+  echo "Enabling autologin and X11 server start"
+  sudo mkdir -p /etc/systemd/system/getty@tty1.service.d/
+  echo "[Service]
+  ExecStart=
+  ExecStart=-/usr/bin/agetty --autologin $USER --noclear %I 38400 linux
+  " | sudo tee /etc/systemd/system/getty@tty1.service.d/override.conf > /dev/null
+  echo '[[ -z $DISPLAY && $XDG_VTNR -eq 1 ]] && exec startx' > ~/.zprofile
+fi
+
+read -p "Are you use virtualbox? <y/N> " prompt
+if [[ $prompt == "y" || $prompt == "yes" || $prompt == "Y"  || $prompt == "Yes" ]]
+then
+  sudo pacman -S virtualbox-guest-utils xf86-video-vesa
+
+  echo "vboxguest
+  vboxsf
+  vboxvideo
+  " | sudo tee /etc/modules-load.d/vbox.conf > /dev/null
+  
+  sed -i '1iexec_always --no-startup-id VBoxClient-all' ~/.i3/config
+else
+  true
+fi
 
 echo "Done!"
 echo "You may want to unlogin (to get zsh works), x server starts automaticly"

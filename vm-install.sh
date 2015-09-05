ca491eed96682ea04a16345e95502b225b6c20d2
indesege <indesege@gmail.com>
2015-09-06 02:23:12 +0300

    vm-install script base

---
+++ vm-install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+set -e
+
+echo "Starting raw installation for VM"
+
+echo "Make partitions on disk"
+parted /dev/sda mklabel msdos
+parted /dev/sda mkpart primary ext4 0% 100%
+parted /dev/sda set 1 boot on
+
+echo "Making filesystem"
+mkfs.ext4 /dev/sda1 
+
+echo "Mounting"
+mount /dev/sda1 /mnt
+
+echo "Setting up base packages"
+pacstrap -i /mnt base base-devel
+
+echo "Generating fstab"
+genfstab -U /mnt > /mnt/etc/fstab
+
+echo "Chrooting into new system"
+echo "@TODO"
+

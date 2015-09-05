45844ac61e889ac2ecae0f0fb8c759eb1792e4c1
indesege <indesege@gmail.com>
2015-09-06 02:35:52 +0300

    vm-install semi-ready

--- vm-install.sh
+++ vm-install.sh
@@ -22,5 +22,27 @@ echo "Generating fstab"
 genfstab -U /mnt > /mnt/etc/fstab
 
 echo "Chrooting into new system"
-echo "@TODO"
+arch-chroot /mnt /bin/bash -x <<'EOF'
+su -
+
+echo "Setting up locales"
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+locale-gen
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+export LANG=en_US.UTF-8
+
+echo "Setting up time"
+ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime
+
+echo "Setting up bootloader"
+yes "" | pacman -S git grub
+grub-install --recheck /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+exit
+EOF
+
+umount -R /mnt
+
+echo "Done, please reboot to continue installation"
 
